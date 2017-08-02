1d77463ce04819067416f1492c9483c20ab4eea5
John Michael McCasland <jmmccasland@gmail.com>
2017-08-02 15:50:51 -0500

    Add better map

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import 'whatwg-fetch';
 
+
 // Open and close menu on .menu__btn click
 const open = document.querySelector('.menu__btn');
 const close = document.querySelector('.menu__overlay__close');
 const overlay = document.querySelector('.menu__overlay');
-
 
 open.addEventListener('click', () => {
   overlay.style.display = 'flex';
