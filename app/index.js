c8c58e0941a86b7abf730f3395997d2a87c9ec15
John Michael McCasland <jmmccasland@gmail.com>
2017-08-03 12:37:01 -0500

    Spotify widget

--- app/index.js
+++ app/index.js
@@ -13,3 +13,24 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
+
+
+// Open Spotify tracklisting
+const spotifyBtn = document.querySelector('.player__button');
+const player = document.querySelector('.player');
+const buttonIcon = document.querySelector('.player__button__icon');
+let isOpen = false;
+
+spotifyBtn.addEventListener('click', () => {
+  if (isOpen) {
+    player.style.bottom = '-220px';
+    buttonIcon.classList.remove('fa-arrow-down');
+    buttonIcon.classList.add('fa-arrow-up');
+    isOpen = !isOpen;
+  } else {
+    player.style.bottom = 0;
+    buttonIcon.classList.remove('fa-arrow-up');
+    buttonIcon.classList.add('fa-arrow-down');
+    isOpen = !isOpen;
+  }
+});
